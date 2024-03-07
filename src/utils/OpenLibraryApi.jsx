cec9e7387d0c7c9d820657a16bed276a94ffda1a
Scrobwofl <scrobwofl@gmail.com>
2024-03-07 11:25:41 +0000

    Changes for vercel

--- src/utils/OpenLibraryApi.jsx
+++ src/utils/OpenLibraryApi.jsx
@@ -1,4 +1,4 @@
-export async function getBookDetails(id) {
+export default async function getBookDetails(id) {
   const response = await fetch(`https://openlibrary.org/books/${id}.json`);
   const data = await response.json();
 
